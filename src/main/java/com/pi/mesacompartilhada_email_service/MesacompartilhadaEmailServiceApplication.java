a12073d0e978176ad814956ecc0da8acdad294f7
willotavio <89710130+willotavio@users.noreply.github.com>
2025-03-24 21:24:14 -0300

    Criando projeto

---
+++ src/main/java/com/pi/mesacompartilhada_email_service/MesacompartilhadaEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pi.mesacompartilhada_email_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesacompartilhadaEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesacompartilhadaEmailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/mesacompartilhada_email_service/MesacompartilhadaEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.mesacompartilhada_email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesacompartilhadaEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
